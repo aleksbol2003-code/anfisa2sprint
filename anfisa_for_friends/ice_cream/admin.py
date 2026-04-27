818dd683cfbaba14fddac347791e0dbcb2a65698
Aleks Bolotin <aleksbol2003@rambler.ru>
2026-04-27 13:40:50 +0900

    admin_2

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,14 +1,47 @@
+# ice_cream/admin.py
 from django.contrib import admin
 
-# Register your models here.
-# Из модуля models импортируем модель Category...
-from .models import Category
-from .models import Topping
-from .models import Wrapper
-from .models import IceCream
+from .models import Category, IceCream, Topping, Wrapper
 
-# ...и регистрируем её в админке:
-admin.site.register(Category)
+admin.site.empty_value_display = 'Не задано'
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('is_published',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+# Регистрируем класс с настройками админки для моделей IceCream и Category:
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Category, CategoryAdmin)
+# Регистрируем модели Topping и Wrapper,
+# чтобы ими можно было управлять через админку
+# (интерфейс админки для этих моделей останется стандартным):
 admin.site.register(Topping)
 admin.site.register(Wrapper)
-admin.site.register(IceCream)
